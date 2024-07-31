d7fc1d8ec0c45eeb4f3e2305a1564c5449e88fe6
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2024-07-31 17:57:27 +0300

    Refactor AddSimpleProduct patch

--- Scandiweb/Test/Setup/Patch/Data/AddSimpleProduct.php
+++ Scandiweb/Test/Setup/Patch/Data/AddSimpleProduct.php
@@ -2,6 +2,7 @@
 
 namespace Scandiweb\Test\Setup\Patch\Data;
 
+use Exception;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
@@ -11,6 +12,9 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\InputException;
 use Magento\Framework\Exception\StateException;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
 
 class AddSimpleProduct implements DataPatchInterface
 {
@@ -35,29 +39,46 @@ class AddSimpleProduct implements DataPatchInterface
     protected State $state;
 
     /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSave;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
      * AddSimpleProduct constructor.
+     *
      * @param ProductInterfaceFactory $productFactory
      * @param ProductRepositoryInterface $productRepository
      * @param StoreManagerInterface $storeManager
      * @param State $state
+     * @param SourceItemsSaveInterface $sourceItemsSave
+     * @param SourceItemInterfaceFactory $sourceItemFactory
      */
     public function __construct(
         ProductInterfaceFactory $productFactory,
         ProductRepositoryInterface $productRepository,
         StoreManagerInterface $storeManager,
-        State $state
-    )
-    {
+        State $state,
+        SourceItemsSaveInterface $sourceItemsSave,
+        SourceItemInterfaceFactory $sourceItemFactory
+    ) {
         $this->productInterfaceFactory = $productFactory;
         $this->productRepository = $productRepository;
         $this->storeManager = $storeManager;
         $this->state = $state;
+        $this->sourceItemsSave = $sourceItemsSave;
+        $this->sourceItemFactory = $sourceItemFactory;
     }
 
     /**
      * Applies the patch
+     *
      * @return void
-     * @throws \Exception
+     * @throws Exception
      */
     public function apply(): void
     {
@@ -66,6 +87,7 @@ class AddSimpleProduct implements DataPatchInterface
 
     /**
      * Execute the patch
+     *
      * @return void
      * @throws NoSuchEntityException
      * @throws CouldNotSaveException
@@ -82,34 +104,47 @@ class AddSimpleProduct implements DataPatchInterface
         }
 
         // Product does not exist, create a new one
-        $product->setSku($sku);
-
-        // Set or update product attributes
-        $product->setName('Oversize T-Shirt');
-        $product->setAttributeSetId(4); // Default attribute set ID
-        $product->setStatus(1); // Enabled
-        $product->setWeight(10);
-        $product->setVisibility(4); // Catalog, Search
-        $product->setTaxClassId(0); // None
-        $product->setTypeId('simple');
-        $product->setPrice(100);
-        $product->setStockData(
-            [
-                'qty' => 100,
-                'is_qty_decimal' => 0,
-                'is_in_stock' => 1,
-            ]
-        );
-        $product->setWebsiteIds([$this->storeManager->getStore()->getWebsiteId()]);
-        $product->setCategoryIds([3]); // 3 for Men
-        $product->setQuantity(100);
+        $product->setSku($sku)
+            ->setName('Oversize T-Shirt')
+            ->setAttributeSetId(4) // Default attribute set ID
+            ->setStatus(1) // Enabled
+            ->setWeight(10)
+            ->setVisibility(4) // Catalog, Search
+            ->setTaxClassId(0) // None
+            ->setTypeId('simple')
+            ->setPrice(100)
+            ->setStockData(
+                [
+                    'qty' => 100,
+                    'is_qty_decimal' => 0,
+                    'is_in_stock' => 1,
+                ]
+            )
+            ->setWebsiteIds([$this->storeManager->getStore()->getWebsiteId()])
+            ->setCategoryIds([3]); // 3 for Men
 
         // Save the product
         $this->productRepository->save($product);
+
+        // Set source item quantity
+        $this->setSourceItemQuantity($sku, 'default', 100);
+    }
+
+    private function setSourceItemQuantity(string $sku, string $sourceCode, float $quantity): void
+    {
+
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSku($sku)
+            ->setSourceCode($sourceCode)
+            ->setQuantity($quantity)
+            ->setStatus(Status::STATUS_ENABLED);
+
+        $this->sourceItemsSave->execute([$sourceItem]);
     }
 
     /**
      * Get the array of patches that have to be executed before this patch
+     *
      * @return array|string[]
      */
     public static function getDependencies(): array
@@ -119,6 +154,7 @@ class AddSimpleProduct implements DataPatchInterface
 
     /**
      * Get the array of patches that have to be executed after this patch
+     *
      * @return array|string[]
      */
     public function getAliases(): array
